ce0e71bc44e81ea67f3b667826f0c32229146bb3
katianavarez <katia.navarez252855@potros.itson.edu.mx>
2025-05-16 22:30:15 -0700

    m
    m

---
+++ capaPersistenciaBanco/src/main/java/org/persistenciabanco/PersistenciaBanco.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.persistenciabanco;
+
+/**
+ *
+ * @author katia
+ */
+public class PersistenciaBanco {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ capaPresentacionBanco/src/main/java/org/capapresentacionbanco/CapaPresentacionBanco.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.capapresentacionbanco;
+
+/**
+ *
+ * @author katia
+ */
+public class CapaPresentacionBanco {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
